e46aabfcb58b3b3878ec5c1f4f715e1b96bc6b41
YasabnehTA-Inno <265933360+YasabnehTA-Inno@users.noreply.github.com>
2026-07-24 16:19:09 +0300

    refactor: remove unused DesignSystem and t imports from ScoreboardQuestionScreen

--- src/ui/screens/ScoreboardQuestionScreen.ts
+++ src/ui/screens/ScoreboardQuestionScreen.ts
@@ -1,7 +1,6 @@
-import { DesignSystem } from "../theme/DesignSystem";
 import { UIManager } from '../../core/managers/UIManager';
 import { AudioManager } from '../../core/managers/AudioManager';
-import { i18n, t } from '../../localization/i18n';
+import { i18n } from '../../localization/i18n';
 import { QuizEngine, MatchStats } from '../../core/quiz/QuizEngine';
 import { Competition } from '../../core/quiz/CompetitionRegistry';
 import { GameSessionManager, GameSession } from '../../core/quiz/GameSessionManager';
